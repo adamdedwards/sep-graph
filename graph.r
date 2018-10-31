9e69fdd12926cfdc94821650a68c8a205c7283b4
adamdedwards <3643320+adamdedwards@users.noreply.github.com>
2018-10-31 10:50:13 -0500

    add graphs

--- graph.r
+++ graph.r
@@ -2,32 +2,39 @@
 library(networkD3)
 library(igraph)
 library(magrittr)
+library(threejs)
 
-#################### Load data
+##############################     Load Data    ##############################
 
 nodes <- read.csv("data/win1997_node_list.txt", header = TRUE)
 edges <- read.csv("data/win1997_edge_list.txt", header = TRUE)
 
-#################### Create Graphs
+##############################  Create Graphs   ##############################
 
-gg <- graph_from_edgelist(matrix(unlist(lapply(edges, as.character)), ncol = 2, byrow = TRUE), directed = FALSE)
-gg <- simplify(gg, remove.multiple = TRUE, remove.loops = TRUE,edge.attr.comb = igraph_opt("edge.attr.comb"))
+gg <- graph_from_edgelist(as.matrix(edges), directed = TRUE)                 # Load edge csv data.frame into an igraph object
+gg <- simplify(gg, remove.multiple = TRUE, remove.loops = TRUE)              # Simplify igraph object; remove dups and loops
 
-members <- membership(cluster_fast_greedy(gg))
+############################## Graph Attributes ##############################
 
-graph_d3 <- igraph_to_networkD3(gg,group = members)
+i <- cluster_walktrap(gg)$membership
+gg <- set_vertex_attr(gg,"group",index = V(gg),i)                            # Create a "group" attribute and load it into node attributes
 
+c <- rainbow(max(V(gg)$group))
+gg <- set_vertex_attr(gg, "color",index = V(gg),c[i])                        # Add node colors based on group membership
+
+graph_d3 <- igraph_to_networkD3(gg,group = i)
 graph_d3$nodes$degree <- as.character(degree(gg, v = V(gg)))
 graph_d3$nodes$btwn <- as.character(betweenness(gg, v = V(gg), directed = TRUE))
 
-# Plot
-save <- forceNetwork(Links = graph_d3$links,
+##############################        Plot      ############################## 
+
+forceNetwork(Links = graph_d3$links,
              Nodes = graph_d3$nodes,
              Source = "source",
              Target = "target",
              Group = "group",
              NodeID = "name", 
-             Nodesize = "btwn",
+             Nodesize = "degree",
              charge = -50,
              arrows = FALSE,
              legend = FALSE,
@@ -39,4 +46,11 @@ save <- forceNetwork(Links = graph_d3$links,
              zoom = TRUE
              ) 
 
-saveNetwork(save, file = 'Net1.html')
+
+graph <- graphjs(gg)
+
+graphjs(gg, layout=layout_with_fr(gg, dim=3))
+
+gg  <- set_edge_attr(gg,"color", value = "#000000")
+gg  <- set_edge_attr(gg, "weight", value=10)
+
