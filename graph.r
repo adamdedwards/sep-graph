8928ea8435061789ed8f733c8ee64fdc60fa79d6
adamdedwards <3643320+adamdedwards@users.noreply.github.com>
2018-10-30 23:25:15 -0500

    add 1997 graph

--- graph.r
+++ graph.r
@@ -1,6 +1,7 @@
 
 library(networkD3)
 library(igraph)
+library(magrittr)
 
 #################### Load data
 
@@ -9,9 +10,10 @@ edges <- read.csv("data/win1997_edge_list.txt", header = TRUE)
 
 #################### Create Graphs
 
-gg <- graph_from_edgelist(matrix(unlist(lapply(edges, as.character)), ncol = 2, byrow = TRUE), directed = TRUE)
+gg <- graph_from_edgelist(matrix(unlist(lapply(edges, as.character)), ncol = 2, byrow = TRUE), directed = FALSE)
+gg <- simplify(gg, remove.multiple = TRUE, remove.loops = TRUE,edge.attr.comb = igraph_opt("edge.attr.comb"))
 
-members <- membership(cluster_optimal(gg))
+members <- membership(cluster_fast_greedy(gg))
 
 graph_d3 <- igraph_to_networkD3(gg,group = members)
 
@@ -19,22 +21,22 @@ graph_d3$nodes$degree <- as.character(degree(gg, v = V(gg)))
 graph_d3$nodes$btwn <- as.character(betweenness(gg, v = V(gg), directed = TRUE))
 
 # Plot
-forceNetwork(Links = graph_d3$links,
+save <- forceNetwork(Links = graph_d3$links,
              Nodes = graph_d3$nodes,
              Source = "source",
              Target = "target",
              Group = "group",
-             height = 500,
-             width = 750,
              NodeID = "name", 
              Nodesize = "btwn",
              charge = -50,
              arrows = FALSE,
              legend = FALSE,
-             bounded = TRUE,
+             bounded = FALSE,
              opacity = 1,
              fontFamily = "sans-serif",
              fontSize = 14,
-             opacityNoHover = .2,
+             opacityNoHover = 0,
              zoom = TRUE
-             )
+             ) 
+
+saveNetwork(save, file = 'Net1.html')
