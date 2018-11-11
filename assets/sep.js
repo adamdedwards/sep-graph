708cd2991a7d81c30555d72c639e6a02114a4457
adamdedwards <3643320+adamdedwards@users.noreply.github.com>
2018-11-11 17:39:05 -0600

    add javascript

--- assets/sep.js
+++ assets/sep.js
@@ -4,11 +4,14 @@ var community = 2;
 window.onload = function(){document.getElementById('comm_frame').src = "pages/graphs/community_5.html";};
 
 function prevcomm() {
-    community = community - 1;
+    if(community > 1) { community = community - 1; }
+    else { community = 1; }
+
     document.getElementById('comm_frame').src = "pages/graphs/community_"+community+".html";
 }
 
 function nextcomm() {
-    community = community + 1;
+  if(community <= 19) { community = community + 1; }
+  else { community = 18; }
     document.getElementById('comm_frame').src = "pages/graphs/community_"+community+".html";
 }
